3ce07d46f4d47c29b40588be031fb74e3a1662ed
Mark Janssen <mpc.janssen@gmail.com>
2020-12-15 10:51:33 +0000

    Days

--- 2020/setup.tcl
+++ 2020/setup.tcl
@@ -1,4 +1,5 @@
 lappend auto_path [file dirname [info script]]/lib
+lappend auto_path /usr/lib/tcltk/x86_64-linux-gnu/
 tcl::tm::path add [file dirname [info script]]/modules
 tcl::tm::path add [file dirname [info script]]/../modules
 package forget aoc
